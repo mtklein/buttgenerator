572ad1293a209e4b8b48686b0153a4ca370b4d37
Mike Klein <mtklein@gmail.com>
2012-10-10 19:56:57 -0700

    background + favicon

--- hello/hello.go
+++ hello/hello.go
@@ -9,6 +9,29 @@ func init() {
 	http.HandleFunc("/", handler)
 }
 
+const html = `
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>Butt Generator</title>
+
+		<style>
+		html {
+			background: url(images/butt.jpg) no-repeat center center fixed;
+			-webkit-background-size: cover;
+			-moz-background-size: cover;
+			-o-background-size: cover;
+			background-size: cover;
+		}
+		</style>
+	</head>
+
+	<body> </body>
+
+</html>
+`
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, Le.  WE HAVE THE POWER.")
+	fmt.Fprintf(w, html)
 }
