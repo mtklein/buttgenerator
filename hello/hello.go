28d22d2f7e4c41f1237826e0e7ec153d65d649cd
Mike Klein <mtklein@gmail.com>
2012-10-11 09:06:07 -0700

    svg

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,8 @@
 package hello
 
 import (
-	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 )
 
@@ -9,29 +10,35 @@ func init() {
 	http.HandleFunc("/", handler)
 }
 
-const html = `
+var html = template.Must(template.New("html").Parse(`
 <!DOCTYPE html>
 <html>
-
 	<head>
 		<title>Butt Generator</title>
-
 		<style>
-		html {
-			background: url(images/butt.jpg) no-repeat center center fixed;
-			-webkit-background-size: cover;
-			-moz-background-size: cover;
-			-o-background-size: cover;
-			background-size: cover;
-		}
+			html {
+				background: url({{.Image}}) no-repeat center center fixed;
+				background-size: cover;
+			}
 		</style>
 	</head>
-
-	<body> </body>
-
 </html>
-`
+`))
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, html)
+	type Args struct {
+		Image template.URL
+	}
+
+	image := "data:image/svg+xml;utf8," + butt_inline
+	if (r.URL.Path == "/original") {
+		image = "images/butt.jpg"
+	}
+	args := Args {
+		Image: template.URL(image),
+	}
+
+	if err := html.Execute(w, args); err != nil {
+		log.Println("error executing html: ", err)
+	}
 }
