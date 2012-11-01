ab08308b5e6c1edad1d01d69cca1d65b045d274b
Le Wei <le.giantsquid.wei@gmail.com>
2012-10-31 23:17:35 -0700

    SVG annotations

--- buttgenerator/butt_inline.go
+++ buttgenerator/butt_inline.go
@@ -6,14 +6,23 @@ import (
 
 var butt_inline = strings.Replace(`
 <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="640px" height="480px" viewBox="0 0 640 480" enable-background="new 0 0 640 480" xml:space="preserve">
+// maybe nothing
 <path fill="none" stroke="#00093F" stroke-miterlimit="10" d="M171,91"/>
+// butt
 <path fill="#DEC276" d="M171,91l291-16c0,0,34,98,28,120s-16,72-54,80s-72.29,6-105.145-36c0,0-14.855,74-64.855,64S156,277,171,91z "/>
+// right arm
 <polyline fill="none" stroke="#00093F" stroke-width="4" stroke-miterlimit="10" points="514.667,202.333 502.667,197 544,135 486.311,141.616 "/>
+// left arm
 <polyline fill="none" stroke="#00093F" stroke-width="4" stroke-miterlimit="10" points="171,294.333 181.333,273 96.666,269 168.657,199.667 "/>
+// left leg
 <path fill="none" stroke="#00093F" stroke-width="4" stroke-miterlimit="10" d="M268,405l40-18c0,0-24-55.333-20-79.333"/>
+// right leg
 <path fill="none" stroke="#00093F" stroke-width="4" stroke-miterlimit="10" d="M408.666,386.667c0,0,26-12.666,28-12 c0,0-15.999-91.334-3.333-91.334"/>
+// mouth
 <path fill="none" stroke="#001F0E" stroke-width="2" stroke-miterlimit="10" d="M300,189.465c0,0,4,12.201,24.666,10.202 c20.667-2,20.001-10.202,20.001-10.202"/>
+// right eye
 <path fill="#1F003F" stroke="#001F0E" stroke-miterlimit="10" d="M356.667,127c0,0,5.333,2,3.333,8S347.334,130.333,356.667,127z"/>
+// left eye
 <path fill="#13003F" stroke="#001F0E" stroke-miterlimit="10" d="M252.667,147c0,0,10.667,2.667,2.667,9.333 S244,148.333,252.667,147z"/>
 </svg>`,
 "\n", "", -1)
